0e85f408cbf1b258ee67fb611804090770eb1067
Chad Emery <chade2540@gmail.com>
2024-11-02 16:43:27 -0400

    basic logic

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
 import requests
+import yaml
 
-APP_URL = "github.com"
+APP_URL = "https://raw.githubusercontent.com/ChadPunk/python-remote-updater/refs/heads/main/settings.yaml"
 CURRENT_VERSION = "1.0.0"
 
 def updateApplication():
     print('Running update!')
 
 def checkForUpdate(url, version):
-    REPO_CURRENT_VERSION = "1.0.0"
-    print(f'Current version from repo: {REPO_CURRENT_VERSION}')
-    if version != REPO_CURRENT_VERSION:
-        print(f'Versions do not match!\nOur version: {version}\nRepo version {REPO_CURRENT_VERSION}')
-        updateApplication()
-    else:
-        print('Nothing to do!')
-
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        settings = yaml.safe_load(response.text)
+        REPO_CURRENT_VERSION = settings['version']
+        print(f'Current version from repo: {REPO_CURRENT_VERSION}')
+        
+        if version != REPO_CURRENT_VERSION:
+            print(f'Versions do not match!\nOur version: {version}\nRepo version: {REPO_CURRENT_VERSION}')
+            updateApplication()
+        else:
+            print('Nothing to do!')
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching update information: {e}")
+    except yaml.YAMLError as e:
+        print(f"Error parsing YAML file: {e}")
 
 if __name__ == '__main__':
-    checkForUpdate(APP_URL,CURRENT_VERSION)
-
-
-
+    checkForUpdate(APP_URL, CURRENT_VERSION)
